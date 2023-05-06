272bed3322439850a5299c9c9287494341c1e0ad
Krzysztof Glowacz <krzysztofglowacz88@gmail.com>
2023-05-06 22:18:43 +0200

    Deleted unused source files.

--- lab_3/lab_2_g.py
+++
@@ -1,9 +0,0 @@
-import sys, datetime
-
-def print_entries(tupleList):
-    for entry in tupleList:
-        print(entry)
-
-if __name__ == "__main__":
-    tupleList = [eval(line) for line in sys.stdin.readlines()]
-    print_entries(tupleList)
